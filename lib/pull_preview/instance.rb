8205acba0f6f067aec6f4dc50055776256eb183d
mfittko <manuel.fittko@sofatutor.com>
2022-07-11 19:07:33 +0200

    temporarily use info

--- lib/pull_preview/instance.rb
+++ lib/pull_preview/instance.rb
@@ -328,7 +328,7 @@ module PullPreview
       [key_file_path].each{|file| FileUtils.chmod 0600, file}
 
       cmd = "ssh #{"-v " if logger.level == Logger::DEBUG}-o ServerAliveInterval=15 -o IdentitiesOnly=yes -i #{key_file_path} #{ssh_address} #{ssh_options.join(" ")}"
-      logger.debug cmd
+      logger.info cmd
 
       system("rsync -Pav --delete -e '#{cmd}' #{source} #{ssh_address}:#{target} --exclude-from=.dockerignore")
     end
